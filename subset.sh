107bc492063844e1b8e0fb7dfa45b5a37e21984f
mtpatter <benhuynh3@gmail.com>
2015-04-13 17:40:25 -0500

    readding files

---
+++ subset.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Author: Ben Huynh
+#This script subsets a series of hyperion band images based on geographic coordinates, creating a new folder of subsetted band images.
+#Usage: ./subset.sh sceneid RBXXX xoff yoff xsize ysize
+FOLDER=/glusterfs/users/bhuynh/cavsarps/$1
+cd $FOLDER
+mkdir $2
+cp *_MTL_L1T.TXT $2
+for f in *.TIF
+do 
+    id=$(basename $f _L1T.TIF)
+    echo "Subsetting image for: $id"
+    output=${FOLDER}/$2/${id}_${2}_L1T.TIF
+    echo "Output: $output"
+    gdal_translate -projwin $3 $4 $5 $6 $f $output
+done
